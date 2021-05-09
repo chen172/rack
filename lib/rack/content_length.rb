2eb9bea9a0d43f2582487072a36787b6a770ced6
chen <3217855084@qq.com>
2021-05-09 18:19:33 +0800

    Update content_length.rb

--- lib/rack/content_length.rb
+++ lib/rack/content_length.rb
@@ -6,6 +6,7 @@ module Rack
   # a Content-Length or Transfer-Encoding header.  Note that this
   # does not fix responses that have an invalid Content-Length
   # header specified.
+  # 为在响应还没有设置Content-Length或者Transfer-Encoding头部的设置Content-Length头部。
   class ContentLength
     include Rack::Utils
 
@@ -17,12 +18,15 @@ module Rack
       status, headers, body = @app.call(env)
       headers = HeaderHash[headers]
 
+      # 没有设置Content-Length和Transfer-Encoding头部
       if !STATUS_WITH_NO_ENTITY_BODY.key?(status.to_i) &&
          !headers[CONTENT_LENGTH] &&
          !headers[TRANSFER_ENCODING]
 
         obody = body
+        # 初始化长度
         body, length = [], 0
+        # 得到返回的内容体的长度
         obody.each { |part| body << part; length += part.bytesize }
 
         body = BodyProxy.new(body) do
